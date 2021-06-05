5b2206a40eff2e21415885aedcd3d7d224911193
Rishabh Mewar <rmewar.dev@gmail.com>
2021-06-05 09:55:52 +0530

    Create "Order Screen"

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,6 +1,10 @@
 import { createStore, compose, applyMiddleware, combineReducers } from 'redux';
 import thunk from 'redux-thunk';
 import { cartReducer } from './reducers/cartReducer';
+import {
+  orderCreateReducer,
+  orderDetailsReducer,
+} from './reducers/orderReducers';
 import { productDetailsReducer, productListReducer } from './reducers/productReducers';
 import {
   userRegisterReducer,
@@ -34,6 +38,7 @@ const reducer = combineReducers({
   cart: cartReducer,
   userSignin: userSigninReducer,
   userRegister: userRegisterReducer,
+  orderDetails: orderDetailsReducer,
 });
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(
