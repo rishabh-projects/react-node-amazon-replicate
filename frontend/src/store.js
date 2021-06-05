298de04a9c82ad7fc7f90cdfb15f595cb92efa3c
Rishabh Mewar <rmewar.dev@gmail.com>
2021-06-05 08:25:27 +0530

    Create "Shipping Screen"

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -19,7 +19,11 @@ const initialState = {
     cartItems: localStorage.getItem('cartItems')
       ? JSON.parse(localStorage.getItem('cartItems'))
       : [],
+    shippingAddress: localStorage.getItem('shippingAddress')
+      ? JSON.parse(localStorage.getItem('shippingAddress'))
+      : {},
   },
+ 
 };
 // const reducer = (state, action) => {
 //   return { products: data.products };
