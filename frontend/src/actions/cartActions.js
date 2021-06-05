52bea1d290303c28775c5fd871aa535ba3ff1e95
Rishabh Mewar <rmewar.dev@gmail.com>
2021-06-05 08:43:44 +0530

    Create "Payment Screen"

--- frontend/src/actions/cartActions.js
+++ frontend/src/actions/cartActions.js
@@ -3,6 +3,7 @@ import {
   CART_ADD_ITEM,
   CART_REMOVE_ITEM,
   CART_SAVE_SHIPPING_ADDRESS,
+  CART_SAVE_PAYMENT_METHOD,
 } from '../constants/cartConstants';
 
 export const addToCart = (productId, qty) => async (dispatch, getState) => {
@@ -19,6 +20,10 @@ export const addToCart = (productId, qty) => async (dispatch, getState) => {
     },
   });
   localStorage.setItem('cartItems', JSON.stringify(getState().cart.cartItems));
+};
+
+export const savePaymentMethod = (data) => (dispatch) => {
+  dispatch({ type: CART_SAVE_PAYMENT_METHOD, payload: data });
 };
 
 export const saveShippingAddress = (data) => (dispatch) => {
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -22,8 +22,8 @@ const initialState = {
     shippingAddress: localStorage.getItem('shippingAddress')
       ? JSON.parse(localStorage.getItem('shippingAddress'))
       : {},
+    paymentMethod: 'PayPal',
   },
- 
 };
 // const reducer = (state, action) => {
 //   return { products: data.products };
